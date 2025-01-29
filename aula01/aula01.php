114427fb9124a6f655a8a7c59d140b504d382f15
Julio cesar <julioomelt@gmail.com>
2025-01-28 21:59:58 -0300

    Atividades incompletas

--- aula01/aula01.php
+++ aula01/aula01.php
@@ -1,12 +1,13 @@
-<?php
-
-echo"slk";
-echo "moduloD"
+<?php 
 
 
+echo "Informe um numero! \n";
+$numero1 = readline();
 
 
+echo "Informe outro numero! \n";
+$numero2 = readline();
 
+$soma = $numero1 + $numero2;
 
-
-?>
+echo "O resultado da soma é $soma";
---
+++ aula02/aula02.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Informe um numero! \n";
+$num1 = readline();
+
+if($num1 %2 == 0){
+echo "Par $num1";
+}else{
+echo "Impar $num1";
+}
+
